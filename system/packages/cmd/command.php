8f3113e584b73e06344a880bbe2d93037898cc9f
Drm Dev <71892583+DemuraAIdev@users.noreply.github.com>
2021-05-12 21:40:44 +0700

    Add files via upload

--- system/packages/cmd/command.php
+++ system/packages/cmd/command.php
@@ -1,38 +1,73 @@
+#!/usr/bin/php
 <?php
 
-namespace cmd;
 
-use Drm\Core\classes\Cli;
-use Drm\Core\DRM;
+use Drm\Core\classes\cli\CLI;
+use Drm\Core\classes\cli\Options;
 
-class command
+class Complex extends CLI
 {
-    public static function init($args)
+
+    /**
+     * Register options and arguments on the given $options object
+     *
+     * @param Options $options
+     * @return void
+     */
+    protected function setup(Options $options)
     {
+        $options->setHelp('Welcome To PHP DRM CLI');
+        $options->registerOption('longflag', 'This is a global flag that applies to all subcommands', 'l');
 
-        // Remove flag options from the main argument list
-        $args = self::_clear_args($args);
+        $options->registerCommand('system', 'The System Command');
+        $options->registerCommand('bar', 'The bar command');
 
+        $options->registerOption('someflag', 'This is a flag only valid for the foo command', 's', false, 'system');
+        $options->registerArgument('file', 'This argument is only required for the foo command', true, 'system');
 
-        if (!isset($args[1])) {
-            if (Cli::option('v', Cli::option('version'))) {
-                Cli::write('Fuel: ' . DRM::version('system'));
-                return;
-            }
-            return;
-        }
+        $options->registerOption(
+            'load',
+            'Another flag only for the bar command, requiring an argument',
+            'l',
+            'input',
+            'bar'
+        );
+
+        $options->registerCommand('compact', 'Display the help text in a more compact manner');
     }
-    protected static function _clear_args($actions = array())
-    {
-        foreach ($actions as $key => $action) {
-            if (substr($action, 0, 1) === '-') {
-                unset($actions[$key]);
-            }
 
-            // get rid of any junk added by Powershell on Windows...
-            isset($actions[$key]) and $actions[$key] = trim($actions[$key]);
+    /**
+     * Your main program
+     *
+     * Arguments and options have been parsed when this is run
+     *
+     * @param Options $options
+     * @return void
+     */
+    protected function main(Options $options)
+    {
+
+        switch ($options->getCmd()) {
+            case 'system':
+                $this->success('The foo command was called');
+                break;
+            case 'bar':
+                $this->success('The bar command was called');
+                break;
+            case 'compact':
+                $options->useCompactHelp();
+                echo $options->help();
+                exit;
+            default:
+                $this->error('No known command was called, we show the default help instead:');
+                echo $options->help();
+                exit;
         }
 
-        return $actions;
+        $this->info('$options->getArgs():');
+        var_dump($options->getArgs());
     }
 }
+
+$cli = new Complex();
+$cli->run();
