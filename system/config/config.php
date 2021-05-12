c91e5ea2bb2df7fd3ec26fd3fd35ca95208e4a1a
Drm Dev <71892583+DemuraAIdev@users.noreply.github.com>
2021-05-12 21:38:30 +0700

    Add Define Fontawesome URL

--- system/config/config.php
+++ system/config/config.php
@@ -2,13 +2,16 @@
 
 namespace Drm\Config;
 
+//Name APP
+define('APP_NAME', 'DRM');
+
 
 //Base URL
-define('URL_BASE', 'public');
-define('URL_PROTOCOL', '//');
-define('URL_SERVER', $_SERVER['HTTP_HOST']);
-define('URL_SUB_FOLDER', str_replace(URL_BASE, '', dirname($_SERVER['SCRIPT_NAME'])));
-define('BASEURL', URL_PROTOCOL . URL_SERVER . URL_SUB_FOLDER);
+define('URL_BASE', 'public'); //public FOlder
+define('URL_PROTOCOL', '//'); //URL Protocol
+define('URL_SERVER', $_SERVER['HTTP_HOST']); //App URL
+define('URL_SUB_FOLDER', str_replace(URL_BASE, '', dirname($_SERVER['SCRIPT_NAME']))); //sub folder
+define('BASEURL', URL_PROTOCOL . URL_SERVER . URL_SUB_FOLDER); //BASE
 
 //date/Time
 define('timezone', 'Asia/Jakarta'); //set timezone for date();
@@ -23,4 +26,7 @@ define('dbhost', 'localhost'); //Nama host
 define('dbname', 'drm_frm'); //Nama database
 define('dbuser', 'root'); //nama user 
 define('dbpass', ''); //password
-define('dbdriver', 'mysql');
+define('dbdriver', 'mysql'); //Database Driver
+
+//Fontawesome 
+define('FONTKIT', 'https://kit.fontawesome.com/bc77b70f00.js'); //default KIT
