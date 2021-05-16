fbe8b9dbc944e87f4033b38061a392912b40f9f0
Drm Dev <71892583+DemuraAIdev@users.noreply.github.com>
2021-05-16 18:48:36 +0700

    Add files via upload

--- system/core/ver.php
+++ system/core/ver.php
@@ -7,13 +7,13 @@ class ver
 
     public static function DrmVer($App)
     {
-        $ver = file_get_contents(CORE . "/version.json");
+        $ver = file_get_contents(ROOT . "version.json");
         $json = json_decode($ver, TRUE);
         echo $json[0][$App];
     }
     public static function ClassVer($App)
     {
-        $ver = file_get_contents(CORE . "/version.json");
+        $ver = file_get_contents(ROOT . "version.json");
         $json = json_decode($ver, TRUE);
         echo $json[1][$App];
     }
